24d67263600968a673ba494600c1838b6c9e3914
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-09-28 11:13:51 +0600

    thunk add

---
+++ src/components/features/api/transactionApi.js
@@ -0,0 +1,21 @@
+import Fetcher from "../../utils/Fetcher"
+
+export const getTransaction = async () => {
+    const { data } = await Fetcher.get('/transactions')
+    return data
+}
+
+export const addTransaction = async (postData) => {
+    const { data } = await Fetcher.post('/transactions', postData)
+    return data
+}
+
+export const editTransaction = async (id, postData) => {
+    const { data } = await Fetcher.put(`/transactions/${id}`, postData)
+    return data
+}
+
+export const deleteTransaction = async (id) => {
+    const { data } = await Fetcher.delete(`/transactions/${id}`)
+    return data
+}
